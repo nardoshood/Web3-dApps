a4dbbbe3c8a130c0cc5cf8cab4f7330ec687f0a2
nardoshood <nardostilahun83@gmail.com>
2022-06-20 00:54:46 +0300

    Certificate route added

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./components/Main";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import Cert from "./components/cert"
 
 function App() {
 	const user = localStorage.getItem("token");
 
 	return (
 		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}			<Route path="/signup" exact element={<Signup />} />
+			{user && <Route path="/" exact element={<Main />} />}	
+			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
+			<Route path="/cert" exact element={<Cert/>}/>
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			
 		</Routes>
 	);
 }
